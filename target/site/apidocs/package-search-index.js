9944ac23f04458b22bb84df20002b0b239c7fef1
Kenzo Takeda <gkenzot@gmail.com>
2024-05-23 19:22:35 -0300

    atualizacao readme e javadoc

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.com.senac.cenaflix_podcast"},{"l":"br.com.senac.cenaflix_podcast.gui"},{"l":"br.com.senac.cenaflix_podcast.login"},{"l":"br.com.senac.cenaflix_podcast.persistencia"}];updateSearchResults();
